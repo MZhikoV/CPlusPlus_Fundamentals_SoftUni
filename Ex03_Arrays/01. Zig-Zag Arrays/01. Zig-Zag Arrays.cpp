8553fcc45ec9d75c823ed78cbda767890126c28b
MZhikoV <m.zhikov@gmail.com>
2023-11-22 08:02:09 +0200

    solution varieties

--- Ex03_Arrays/01. Zig-Zag Arrays/01. Zig-Zag Arrays.cpp
+++ Ex03_Arrays/01. Zig-Zag Arrays/01. Zig-Zag Arrays.cpp
@@ -19,14 +19,34 @@ int main()
     int arr1[MAXNUM] = {};
     int arr2[MAXNUM] = {};
 
-    for (int i = 1; i <= n; i++) {
+
+    //Lecturer solution
+    int* first = arr1;
+    int* second = arr2;
+
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+
+        first[i] = a;
+        second[i] = b;
+
+        //swap places for each iteration
+        int* temp = first;
+        first = second;
+        second = temp;
+    }
+    
+    //My solution
+    
+    /*for (int i = 1; i <= n; i++) {
         if (i % 2 == 0) {
             cin >> arr2[i - 1] >> arr1[i - 1];
         }
         else {
             cin >> arr1[i - 1] >> arr2[i - 1];
         }
-    }
+    }*/
 
     printArr(arr1, n);
     printArr(arr2, n);
--- Ex03_Arrays/2. Longest Sequence/2. Longest Sequence.cpp
+++ Ex03_Arrays/2. Longest Sequence/2. Longest Sequence.cpp
@@ -15,31 +15,60 @@ int main()
         cin >> arrI[i];
     }
 
-    int seqCnt = 1, idx = 0, seqOld=1, lastidx=0;
+    //Lecturer solution
 
-    for (int i = 0; i < n-1; i++) {
-        if (arrI[i] == arrI[i + 1]) {
-            seqCnt++;
-            if (seqOld <= seqCnt) {
-                idx = i + 1;
-                seqOld = seqCnt;
-            }
+    size_t seqCnt = 1;
+    int   longSeqEl = arrI[0];
+
+    size_t curSeqCnt = 1;
+    int curSeqEl = arrI[0];
+
+    for (int i = 1; i < n; i++) {       //i=1, because we assigned arrI[0] as first seqence element
+        if (arrI[i] == curSeqEl) {
+            curSeqCnt++;
         }
         else {
-           // seqOld = seqCnt;
-            seqCnt = 1;
-            lastidx = i + 1;
+            curSeqCnt = 1;
+            curSeqEl = arrI[i];
+        }
+
+        if (curSeqCnt >= seqCnt) {
+            seqCnt = curSeqCnt;
+            longSeqEl = curSeqEl;
         }
     }
-    if (seqOld > 1) {
-        for (int j = 1 + idx - seqOld; j <= idx; j++) {
-            cout << arrI[j] << ' ';
-        }
-        cout << endl;
+
+    for (int j = 0; j < seqCnt; j++) {
+        cout << longSeqEl << ' ';
     }
-    else {
-        cout << arrI[lastidx] << endl;
-    }
+    cout << endl;
+
+    //My solution
+    //int seqCnt = 1, idx = 0, seqOld=1, lastidx=0;
+
+    //for (int i = 0; i < n-1; i++) {
+    //    if (arrI[i] == arrI[i + 1]) {
+    //        seqCnt++;
+    //        if (seqOld <= seqCnt) {
+    //            idx = i + 1;
+    //            seqOld = seqCnt;
+    //        }
+    //    }
+    //    else {
+    //       // seqOld = seqCnt;
+    //        seqCnt = 1;
+    //        lastidx = i + 1;
+    //    }
+    //}
+    //if (seqOld > 1) {
+    //    for (int j = 1 + idx - seqOld; j <= idx; j++) {
+    //        cout << arrI[j] << ' ';
+    //    }
+    //    cout << endl;
+    //}
+    //else {
+    //    cout << arrI[lastidx] << endl;
+    //}
     
 
     return 0;
