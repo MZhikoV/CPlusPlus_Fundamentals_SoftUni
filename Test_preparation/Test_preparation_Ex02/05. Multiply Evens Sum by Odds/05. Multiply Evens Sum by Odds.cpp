33176586462e6c041f1855613ca346466b5be8da
MZhikoV <m.zhikov@gmail.com>
2023-12-04 17:52:34 +0200

    more solutions

---
+++ Test_preparation/Test_preparation_Ex02/05. Multiply Evens Sum by Odds/05. Multiply Evens Sum by Odds.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    int dig;
+    
+    int sume = 0, sumo = 0;
+
+    while (a > 0) {
+        dig = a % 10;
+
+        if (dig % 2 == 0) {
+            sume += dig;
+        }
+        else {
+            sumo += dig;
+        }
+
+        a /= 10;
+    }
+
+    double result = sume * sumo;
+
+    cout << result << endl;
+
+    return 0;
+}
